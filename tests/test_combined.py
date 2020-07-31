58f7f007436fb429806b7a20981e2b3920158480
Vincent <vincentwarmerdam@gmail.com>
2020-07-31 22:05:26 +0200

    bugfix

--- tests/test_combined.py
+++ tests/test_combined.py
@@ -12,6 +12,9 @@ def pokemon():
 
 
 def test_no_mutate_query(pokemon):
+    """
+    This was an error that happened in the past.
+    """
     r1 = (
         Clumper(pokemon)
         .keep(lambda d: len(d["type"]) == 2)
@@ -25,3 +28,18 @@ def test_no_mutate_query(pokemon):
     )
 
     assert len(r1) == len(r2)
+
+
+def test_not_all_combinations_matter():
+    data = [
+        {"a": 1, "b": 1, "items": [1, 2], "values": [3, 4]},
+        {"a": 2, "b": 2, "items": [3, 2], "values": [5]},
+    ]
+
+    clumper = Clumper(data).explode(item="items", val="values")
+
+    _ = (
+        clumper.group_by("a", "b")
+        .transform(item=("item", "unique"), values=("val", "unique"))
+        .collect()
+    )
