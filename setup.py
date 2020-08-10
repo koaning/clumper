76cce6d0c89bd57cf9ea38342702349ea1830783
samarpan-rai <samarpan-rai@live.com>
2020-08-10 21:49:35 +0200

    Reformatted

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
+import logging
+import sys
 
-base_packages = []
+base_packages = ["jsonlines>==1.2.0"]
 
-test_packages = [
+test_packages = base_packages + [
     "pytest>=5.4.3",
     "black>=19.10b0",
     "flake8>=3.8.3",
     "pytest-datafiles>=2.0",
 ]
 
-util_packages = ["jupyterlab>=2.2.0", "jsonlines>==1.2.0", "pre-commit>=2.6.0"]
+util_packages = ["jupyterlab>=2.2.0", "pre-commit>=2.6.0"]
 
 docs_packages = [
     "mkdocs>=1.1",
@@ -19,6 +21,9 @@ docs_packages = [
 
 dev_packages = test_packages + util_packages + docs_packages
 
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+log = logging.getLogger()
+log.debug(test_packages)
 setup(
     name="clumper",
     version="0.2.3",
