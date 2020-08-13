a982debba14b062aebb87477b98a11c3c3aef2b1
samarpan-rai <samarpan-rai@live.com>
2020-08-13 20:18:17 +0200

    Not checking if the local jsonl file exists

--- tests/test_read_write/test_read_jsonl.py
+++ tests/test_read_write/test_read_jsonl.py
@@ -1,14 +1,9 @@
 import pytest
-import os
 from clumper import Clumper
 
 
 @pytest.mark.parametrize("lines, expected", [(0, 0), (1, 1), (2, 2)])
 def test_local_read_jsonl_expected(single_local_jsonl_file_path, lines, expected):
-
-    assert os.path.isfile(
-        single_local_jsonl_file_path
-    ), "Given JSON file is not a local file"
     assert (
         len(Clumper.read_jsonl(single_local_jsonl_file_path, lines)) == expected
     ), "The number of lines read is not equal to expected number of lines"
