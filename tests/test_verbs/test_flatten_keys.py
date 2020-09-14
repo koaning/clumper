240c46692f98e5318c4a5b97dff4f81480cd1911
vincent d warmerdam <vincentwarmerdam@gmail.com>
2020-09-14 21:17:55 +0200

    Flatten Keys (#51)
    * feature-added
    
    * feature-added

---
+++ tests/test_verbs/test_flatten_keys.py
@@ -0,0 +1,26 @@
+import pytest
+
+from clumper import Clumper
+
+
+@pytest.mark.parametrize("keyname", ["foo", "bar", "buz"])
+def test_can_rename_key(keyname):
+    """We should be able to change the keyname"""
+    data = {
+        "f1": {"p1": 1, "p2": 2},
+        "f2": {"p1": 3, "p2": 4},
+        "f3": {"p1": 5, "p2": 6},
+    }
+
+    expected = [
+        {"p1": 1, "p2": 2, keyname: "f1"},
+        {"p1": 3, "p2": 4, keyname: "f2"},
+        {"p1": 5, "p2": 6, keyname: "f3"},
+    ]
+
+    assert Clumper(data).flatten_keys(keyname=keyname).collect() == expected
+
+
+def test_len_appropriate_dict_input():
+    """You can pass a dictionary, but then the length should be 1. Not the number of keys."""
+    assert len(Clumper({"a": 1, "b": 2, "c": 3})) == 1
