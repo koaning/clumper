630a1c53e20ad123314b15a20e36713a6ea30fc0
koaning <vincentwarmerdam@gmail.com>
2020-12-16 18:02:04 +0100

    new-logo

---
+++ tests/test_verbs/test_unpack.py
@@ -0,0 +1,28 @@
+from clumper import Clumper
+
+d = [
+    {
+        "download_count": 661467185,
+        "project": "pyyaml",
+        "url": "https://libraries.io/pypi/pyyaml",
+        "data": [
+            {"sourcerank": 25},
+            {"dependents": 5280},
+            {"versions": 24},
+            {"stargazers": 1220},
+            {"network": 281},
+            {"watchers": 50},
+            {"contributors": 31},
+        ],
+    },
+    {
+        "download_count": 640176111,
+        "project": "docutils",
+        "url": "https://libraries.io/pypi/docutils",
+        "data": [{"sourcerank": 15}, {"dependents": 460}, {"versions": 26}],
+    },
+]
+
+
+def test_unpack_dict():
+    assert len(Clumper(d).unpack("data")) == 2
